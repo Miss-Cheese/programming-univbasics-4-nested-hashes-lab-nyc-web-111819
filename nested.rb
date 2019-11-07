8f811085aa871f56f6ba92667c0ce7d164c090a4
Natalie Galligan <nat.galligan@gmail.com>
2019-11-07 00:33:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,7 +132,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] << "Assemby"
+programmer_hash[:dennis_ritchie][:languages] << "Assemlby"
 
 programmer_hash
 
